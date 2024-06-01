a6264232a0c084a80b5beff7188121ea8cf71314
Lithin Adari <157233883+LithinAdari@users.noreply.github.com>
2024-06-01 12:55:39 +0530

    updated messFeedBack

--- controllers/Common.js
+++ controllers/Common.js
@@ -34,3 +34,146 @@ exports.fetchAllHostelData = async(_,res) => {
         })
     }
 }
+
+exports.getMessSessionRatingAndReviews = async (req,res) => {
+    try{
+        const {messHallId,date,session} = req.params.body;
+
+        const startDate = new Date(date);
+        startDate.setHours(0, 0, 0, 0);
+
+        const endDate = new Date(date);
+        endDate.setHours(23, 59, 59, 999);
+
+        if(!messHallId || !date || !session){
+            return res.status(400).json({
+                success : false,
+                message : "All fields are required",
+            })
+        }
+
+        const ratingAndReview = await Prisma.messRatingAndReview.findMany({
+            where : {
+                messHallId : parseInt(messHallId),
+                session : session,
+                createdAt : new Date(date),
+            },
+            select : {
+                rating:true,
+                review:true,
+            }
+        });
+
+        return res.status(200).json({
+            success : true,
+            message : "Fetched the rating and review of the session by date",
+            data:ratingAndReview,
+        })
+    }
+    catch(e){
+        console.log(e);
+        console.error(e);
+        return res.status(500).json({
+            success : false,
+            message : "Something went wrong while fetching the rating and review of the session",
+        })
+    }
+}
+
+exports.getAvgRatingByMessId = async (req,res) => {
+    try{
+        const {messHallId,session} = req.params.body;
+
+        if(!messHallId || !session){
+            return res.status(400).json({
+                success : false,
+                message : "Requried all fields",
+            })
+        }
+
+        const result = await Prisma.messRatingAndReview.findMany({
+            where : {
+                messHallId : parseInt(messHallId),
+                session : session,
+            },
+            avg : {
+                rating : true,
+            }
+        });
+
+        return res.status(200).json({
+            success : true,
+            message : "Successfully fetched the average rating of the particular session",
+            data:result,
+        })
+    }
+    catch(e){
+        console.log(e);
+        console.error(e);
+        return res.status(500).json({
+            success : false,
+            message : "Something went wrong while fetching average rating of the session",
+        })
+    }
+}
+
+exports.getAvgRatingOfSession = async (req,res) => {
+    try{
+        const session = req.params.body;
+
+        if(!session){
+            return res.status(400).json({
+                success : false,
+                message : "Required all fields",
+            })
+        }
+
+        const rating = await Prisma.messRatingAndReview.findMany({
+            where : {
+                session : session,
+            },
+            avg : {
+                rating : true,
+            }
+        });
+
+        return res.status(200).json({
+            success : true,
+            message : "Successfully fetched the average rating of the session",
+            data : rating,
+        });
+    }
+    catch(e){
+        console.log(e);
+        console.error(e);
+        return res.status(500).json({
+            success : false,
+            message : "Unable to fetch average rating based on the session",
+        })
+
+    }
+}
+
+exports.getAllRatingAndReview = async (req,res) => {
+    try{
+
+        const ratindAndReview = await Prisma.messRatingAndReview.findMany({
+            select : {
+                rating : true,
+                review : true,
+            }
+        });
+
+        return res.status(400).json({
+            success : true,
+            message : "Successfully fetched all the rating and review ",
+            ratindAndReview,
+        });
+    }
+    catch(e){
+        return res.status(500).json({
+            success : false,
+            message : "Something went wrong while fetching the rating and review",
+        })
+    }
+}
--- routes/Common.js
+++ routes/Common.js
@@ -5,5 +5,9 @@ const {commonController} = require('../controllers');
 
 router.get("/getAllAnnouncements",commonController.getAllAnnouncements);
 router.get("/fetchAllHostelData",commonController.fetchAllHostelData);
+router.get("/getMessSessionRatingAndReview/:messHallId/:date/:session",commonController.getMessSessionRatingAndReviews);
+router.get("/getAvgRatingByMessId/:messHallId",commonController.getAvgRatingByMessId);
+router.get("/getAvgRatingRatingOfSession/:session",commonController.getAvgRatingOfSession);
+router.get("/getAllRatingAndReview",commonController.getAllRatingAndReview);
 
 module.exports = router;
