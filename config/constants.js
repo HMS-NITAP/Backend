170b319efc6a1ab50747fa85fffb51dc9624f0d6
Abhiram0304 <tanneriabhi@gmail.com>
2025-07-18 00:05:21 +0530

    Test 2

--- config/constants.js
+++ config/constants.js
@@ -3,7 +3,7 @@ const IS_REGISTRATION_ON = true;
 const yearWiseStudentList = {
     "2" : ["422253", "422260"],
     "3" : ["422256", "422102"],
-    "4" : ["422254", "422259"]
+    "4" : ["422254", "422258"]
 }
 
 module.exports = { IS_REGISTRATION_ON, yearWiseStudentList };
